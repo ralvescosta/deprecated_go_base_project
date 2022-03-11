c8104d19fd9676d758bb91d9e8e3dd0166d7ff7d
ralvescosta <rafael.rac.mg@gmail.com>
2022-03-11 19:13:52 -0300

    feat: handler adapter

---
+++ pkg/infra/adapters/handler_adapt.go
@@ -0,0 +1,42 @@
+package adapters
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+
+	"markets/pkg/app/interfaces"
+	httpServer "markets/pkg/infra/http_server"
+
+	"github.com/gin-gonic/gin"
+)
+
+var readAllBody = ioutil.ReadAll
+
+func HandlerAdapt(handler func(httpRequest httpServer.HttpRequest) httpServer.HttpResponse, logger interfaces.ILogger) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		body, err := readAllBody(ctx.Request.Body)
+		if err != nil {
+			logger.Error("[HandlerAdapt] error while read request bytes")
+			ctx.JSON(http.StatusInternalServerError, gin.H{})
+			return
+		}
+		ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+		params := make(map[string]string)
+		for _, param := range ctx.Params {
+			params[param.Key] = param.Value
+		}
+
+		request := httpServer.HttpRequest{
+			Body:    body,
+			Headers: ctx.Request.Header,
+			Params:  params,
+			Ctx:     ctx.Request.Context(),
+		}
+
+		result := handler(request)
+
+		ctx.JSON(result.StatusCode, result.Body)
+	}
+}
---
+++ pkg/infra/adapters/handler_adapt_test.go
@@ -0,0 +1,83 @@
+package adapters
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	httpServer "markets/pkg/infra/http_server"
+	"markets/pkg/infra/logger"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+)
+
+func Test_HandlerAdapter(t *testing.T) {
+	t.Run("should execute correctly", func(t *testing.T) {
+		sut := makeSut()
+
+		sut.adapt(sut.ctx)
+
+		assert.Equal(t, 1, *sut.handlerCalledTimes)
+	})
+
+	t.Run("should return error if some occur when read body", func(t *testing.T) {
+		sut := makeSut()
+
+		readAllBody = func(r io.Reader) ([]byte, error) {
+			return nil, errors.New("Error")
+		}
+		sut.logger.On("Error", "[HandlerAdapt] error while read request bytes", []zap.Field(nil))
+
+		sut.adapt(sut.ctx)
+
+		assert.Equal(t, 0, *sut.handlerCalledTimes)
+		sut.logger.AssertExpectations(t)
+	})
+}
+
+type sutReturn struct {
+	adapt              gin.HandlerFunc
+	logger             *logger.LoggerSpy
+	handlerCalledTimes *int
+	handlerSpy         func(httpRequest httpServer.HttpRequest) httpServer.HttpResponse
+	request            *http.Request
+	ctx                *gin.Context
+}
+
+func makeSut() sutReturn {
+	handlerCalledTimes := 0
+	handlerSpy := func(httpRequest httpServer.HttpRequest) httpServer.HttpResponse {
+		handlerCalledTimes++
+		return httpServer.HttpResponse{}
+	}
+
+	logger := logger.NewLoggerSpy()
+
+	req := &http.Request{
+		Body: ioutil.NopCloser(bytes.NewBuffer([]byte(nil))),
+		Header: http.Header{
+			"op": []string{"op"},
+		},
+	}
+
+	sut := HandlerAdapt(handlerSpy, logger)
+
+	contextMock, _ := gin.CreateTestContext(httptest.NewRecorder())
+	contextMock.Params = []gin.Param{{Key: "key", Value: "value"}}
+	contextMock.Request = req
+
+	return sutReturn{
+		handlerSpy:         handlerSpy,
+		handlerCalledTimes: &handlerCalledTimes,
+		logger:             logger,
+		request:            req,
+		adapt:              sut,
+		ctx:                contextMock,
+	}
+}
