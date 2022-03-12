22452781e0deda3ecef90cb6510ae960581cc97d
ralvescosta <rafael.rac.mg@gmail.com>
2022-03-12 09:12:06 -0300

    feat: market value obj

---
+++ pkg/domain/value_objects/market.go
@@ -0,0 +1,21 @@
+package valueObjects
+
+type MarketValueObjects struct {
+	ID         int
+	Long       int
+	Lat        int
+	Setcens    string
+	Areap      string
+	Coddist    int
+	Distrito   string
+	Codsubpref int
+	Subpref    string
+	Regiao5    string
+	Regiao8    string
+	NomeFeira  string
+	Registro   string
+	Logradouro string
+	Numero     string
+	Bairro     string
+	Referencia string
+}
