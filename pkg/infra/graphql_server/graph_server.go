5c297e5a0218a2557dcaa0ec5391172c4af11d4b
ralvescosta <rafael.rac.mg@gmail.com>
2022-04-22 06:24:18 -0300

    tests: graphql server

--- pkg/infra/graphql_server/graph_server.go
+++ pkg/infra/graphql_server/graph_server.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
@@ -18,12 +18,19 @@ type IGraphqlServer interface {
 	ServeHTTP(w http.ResponseWriter, r *http.Request)
 }
 
-type graphqlServer struct {
-	srv *handler.Server
+type IGqlHandlerServer interface {
+	AddTransport(graphql.Transport)
+	SetQueryCache(graphql.Cache)
+	Use(graphql.HandlerExtension)
+	ServeHTTP(http.ResponseWriter, *http.Request)
 }
 
-func (pst graphqlServer) Default() {
-	pst.srv.AddTransport(&transport.Websocket{
+type graphqlServer struct {
+	srv IGqlHandlerServer
+}
+
+var (
+	ws = &transport.Websocket{
 		Upgrader: websocket.Upgrader{
 			CheckOrigin: func(r *http.Request) bool {
 				return true
@@ -32,30 +39,35 @@ func (pst graphqlServer) Default() {
 			WriteBufferSize: 1024,
 		},
 		KeepAlivePingInterval: 10 * time.Second,
-		InitFunc: func(ctx context.Context, initPayload transport.InitPayload) (context.Context, error) {
-			log.Println("WS::INITFUNC")
-			log.Println(initPayload)
-			return ctx, nil
-		},
-	})
+	}
+)
 
+func (pst graphqlServer) Default() {
+	ws.InitFunc = pst.initFunc
+	pst.srv.AddTransport(ws)
 	pst.srv.AddTransport(transport.Options{})
 	pst.srv.AddTransport(transport.GET{})
 	pst.srv.AddTransport(transport.POST{})
 	pst.srv.AddTransport(transport.MultipartForm{})
 
 	pst.srv.SetQueryCache(lru.New(100))
-
-	pst.srv.Use(extension.Introspection{})
 	pst.srv.Use(extension.AutomaticPersistedQuery{
 		Cache: lru.New(100),
 	})
+
+	pst.srv.Use(extension.Introspection{})
 }
 
 func (pst graphqlServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	pst.srv.ServeHTTP(w, r)
 }
 
-func NewGraphQLServer(srv *handler.Server) IGraphqlServer {
+func (pst graphqlServer) initFunc(ctx context.Context, initPayload transport.InitPayload) (context.Context, error) {
+	log.Println("WS::INITFUNC")
+	log.Println(initPayload)
+	return ctx, nil
+}
+
+func NewGraphQLServer(srv IGqlHandlerServer) IGraphqlServer {
 	return graphqlServer{srv}
 }
