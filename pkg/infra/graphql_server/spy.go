9071c0063c7de785704a057a6a45e2d831e8c6a5
ralvescosta <rafael.rac.mg@gmail.com>
2022-04-22 06:24:22 -0300

    tests: graphql server spy

--- pkg/infra/graphql_server/spy.go
+++ pkg/infra/graphql_server/spy.go
@@ -3,6 +3,7 @@ package graphqlserver
 import (
 	"net/http"
 
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -18,4 +19,28 @@ func (m GraphqlServerSpy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func NewGraphqlServerSpy() *GraphqlServerSpy {
 	return new(GraphqlServerSpy)
+}
+
+type GqlHandlerServerSpy struct {
+	mock.Mock
+}
+
+func (pst *GqlHandlerServerSpy) AddTransport(t graphql.Transport) {
+	pst.Called(t)
+}
+
+func (pst *GqlHandlerServerSpy) SetQueryCache(c graphql.Cache) {
+	pst.Called(c)
+}
+
+func (pst *GqlHandlerServerSpy) Use(h graphql.HandlerExtension) {
+	pst.Called(h)
+}
+
+func (pst *GqlHandlerServerSpy) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	pst.Called(res, req)
+}
+
+func NewGqlHandlerServerSpy() *GqlHandlerServerSpy {
+	return new(GqlHandlerServerSpy)
 }
