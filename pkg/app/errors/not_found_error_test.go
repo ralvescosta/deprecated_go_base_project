df81abe3616c4c04ec01ca0c1aca1d3b918b7fd3
ralvescosta <rafael.rac.mg@gmail.com>
2022-05-28 07:33:48 -0300

    test: improv test

--- pkg/app/errors/not_found_error_test.go
+++ pkg/app/errors/not_found_error_test.go
@@ -3,22 +3,25 @@ package errors
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func Test_NotFoundError(t *testing.T) {
-	t.Run("should create a notFoundError correctly", func(t *testing.T) {
-		err := NewNotFoundError("some error")
-
-		assert.Error(t, err)
-		assert.IsType(t, NotFoundError{}, err)
-	})
+type NotFoundErrorTestSuite struct {
+	suite.Suite
 }
 
-func Test_NotFoundError_Error(t *testing.T) {
-	t.Run("should return error msg", func(t *testing.T) {
-		err := NewNotFoundError("some error")
+func TestNotFoundErrorTestSuite(t *testing.T) {
+	suite.Run(t, new(NotFoundErrorTestSuite))
+}
 
-		assert.Equal(t, "some error", err.Error())
-	})
+func (s *NotFoundErrorTestSuite) TestNotFoundError() {
+	err := NewNotFoundError("some error")
+
+	s.Error(err)
+	s.IsType(NotFoundError{}, err)
+}
+
+func (s *NotFoundErrorTestSuite) TestNotFoundErrorError() {
+	err := NewNotFoundError("some error")
+	s.Equal("some error", err.Error())
 }
